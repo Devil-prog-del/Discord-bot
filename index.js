73bec9c3777dd62fbecffa75a92951a39d7ed91b
Devil-prog-del <leo.hermetz60120@gmail.com>
2026-05-24 16:52:38 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`✅ Bot connecté : ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
